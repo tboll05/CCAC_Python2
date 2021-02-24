a63622189f53ef6841eaf6a842e65de54aed087c
Timothy <fake@email.com>
2021-02-24 18:01:55 -0500

    Comments added

--- DictionaryProject/SummaryDict.py
+++ DictionaryProject/SummaryDict.py
@@ -9,8 +9,11 @@ def buildDictionary(f):
         fileReader = csv.DictReader(file)
 
         for record in fileReader:
+            
+            #Keep track of how many records are in the file
             summaryDict['Total Records'] += 1
 
+            #Add up all the estimated release amounts
             summaryDict['Average Release Amount'] += float(record['REL_EST_AMT'])
 
             if record['FACILITY_NAME'] not in summaryDict['Facility Amount']:
@@ -28,20 +31,23 @@ def buildDictionary(f):
                 summaryDict['Chemical'][record['CHEM_NAME']] += 1
 
             if record['CITY_NAME'] not in summaryDict['City']:
-                #we need to add the chemical name to our dict, with count 1
+                #we need to add the city name to our dict, with count 1
                 summaryDict['City'][record['CITY_NAME']] = 1
             else:
                 #when already in, increment by 1
                 summaryDict['City'][record['CITY_NAME']] += 1
 
+        #Calculate the average esitmated release amount across all facilities
         summaryDict['Average Release Amount'] = round(summaryDict['Average Release Amount'] / summaryDict['Total Records'], 2)
         
+        #Return the final summary dictionary
         return summaryDict
     
-    
+#Function for filtering the top companies with the most estimated release amounts
 def top_N_estimated_facility_release(userDict, n):
     count = 0
     
+    #Loop that will put the company names in opder based on total estimated release amount in descending order
     for item in sorted(userDict['Facility Amount'].items(), key = lambda item: item[1], reverse = True):
         print(item[0] + ":", "{:,}".format(round(item[1],2)))
         count += 1
